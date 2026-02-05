43a2cfd68280bdd2e45a0a8d606ad770d0162acf
Henry Lau <henry.lau@ons.gov.uk>
2026-02-05 16:34:37 +0000

    WIP

--- scatter-bubble-plot-animated/config.js
+++ scatter-bubble-plot-animated/config.js
@@ -1,6 +1,6 @@
 config = {
 
-	"graphicDataURL": "data.csv",
+	"graphicDataURL": "../example-data/bubble-animated.csv",
 	"colourPalette": ONScolours.oceanBlue,
 	"sourceText": "Office for National Statistics",
 	"accessibleSummary": "The chart canvas is hidden from screen readers. The main message is summarised by the chart title and the data behind the chart is available to download below.",
